e81617d66144d933638b151e29bb817f5388283f
DiyawanDawan <diyawan0@gmail.com>
2023-07-03 19:53:26 -0700

    first commit

---
+++ src/scripts/index.js
@@ -0,0 +1,52 @@
+import "regenerator-runtime";
+import "../styles/style.css";
+
+// Fetch DATA.json
+import("../DATA.json").then(({ default: jsonData }) => {
+  console.log(jsonData);
+  let datas = jsonData["restaurants"];
+  let dataList = "";
+  datas.forEach(function (data) {
+    dataList += `
+        <div class="list_item">
+            <img class="list_item_thumb" src="${data["pictureId"]}" alt="${
+      data["name"]
+    }" title="${data["name"]}">
+            <div class="city">${data["city"]}</div>
+            <div class="list_item_content">
+                <p class="list_item_rating">
+                    Rating : 
+                    <a href="#" class="list_item_rating_value">${
+                      data["rating"]
+                    }</a>
+                </p></br>
+                <h1 class="list_item_title"><a href="#">${data["name"]}</a></h1>
+                <div class="list_item_desc">${data["description"].slice(
+                  0,
+                  150
+                )}...</div>
+            </div>
+        </div>
+        `;
+  });
+  document.querySelector("#tes").innerHTML = dataList;
+});
+
+// TODO sletct
+const menu = document.querySelector("#menu");
+const hero = document.querySelector(".hero");
+const main = document.querySelector("main");
+const drawer = document.querySelector("#drawer");
+
+menu.addEventListener("click", function (event) {
+  drawer.classList.toggle("open");
+  event.stopPropagation();
+});
+
+hero.addEventListener("click", function () {
+  drawer.classList.remove("open");
+});
+
+main.addEventListener("click", function () {
+  drawer.classList.remove("open");
+});
